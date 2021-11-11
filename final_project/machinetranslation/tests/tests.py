c908548947f3195382c4c99e8e593a5db7e9471c
drewilson23 <52948836+drewilson23@users.noreply.github.com>
2021-11-10 23:51:43 -0600

    Update tests.py

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,22 +1,22 @@
 import unittest
-from translator import englishToFrench, frenchToEnglish
+from ..translator import english_to_french, french_to_english
 
 class TestTranslator(unittest.TestCase):
     def test_englishToFrench(self):
-        self.assertEqual(englishToFrench('Hello'), 'Bonjour')
-        self.assertEqual(englishToFrench('What time is it?'), 'Quelle heure est-il?')
-        self.assertEqual(englishToFrench('bird'),'Oiseau')
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        self.assertEqual(english_to_french('What time is it?'), 'Quelle heure est-il?')
+        self.assertEqual(english_to_french('bird'),'Oiseau')
     
     def test_frenchToEnglish(self):
-        self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
-        self.assertEqual(frenchToEnglish('Quelle heure est-il?'), 'What time is it?')
-        self.assertEqual(frenchToEnglish('maman'), 'Mom')
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+        self.assertEqual(french_to_english('Quelle heure est-il?'), 'What time is it?')
+        self.assertEqual(french_to_english('maman'), 'Mom')
     
     def test_frenchToEnglishNull(self):
-        self.assertRaises(ValueError, frenchToEnglish, None)
+        self.assertRaises(ValueError, french_to_english, None)
     
     def test_englishToFrenchNull(self):
-        self.assertRaises(ValueError, englishToFrench, None)
+        self.assertRaises(ValueError, english_to_french, None)
 
 if __name__=='__main__':
     unittest.main()
