406eec93b7ece0fdbf85a47796470965a5c66e3b
drewilson23 <52948836+drewilson23@users.noreply.github.com>
2021-11-10 23:53:20 -0600

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,3 +1,6 @@
+"""
+English/French translator
+"""
 import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
@@ -18,23 +21,20 @@ language_translator.set_service_url(
     'https://api.us-south.language-translator.watson.cloud.ibm.com')
 
 
-def englishToFrench(englishText):
+def english_to_french(text1):
     """
     This function translates english to french
     """
-    translation = language_translator.translate(
-        text=englishText,
+    frenchtranslation = language_translator.translate(
+        text=text1,
         model_id='en-fr').get_result()
-    frenchText = translation.get("translations")[0].get("translation")
-    return frenchText
+    return frenchtranslation.get("translations")[0].get("translation")
 
-
-def frenchToEnglish(frenchText):
+def french_to_english(text1):
     """
     This function translates french to english
     """
-    translation = language_translator.translate(
-        text=frenchText,
+    englishtranslation = language_translator.translate(
+        text=text1,
         model_id='fr-en').get_result()
-    englishText = translation.get("translations")[0].get("translation")
-    return englishText
+    return englishtranslation.get("translations")[0].get("translation")
